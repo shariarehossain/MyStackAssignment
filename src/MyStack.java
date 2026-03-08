b7919bfb7ddb3d724758bccd9333fd471d903848
shariarehossain <226770417+shariarehossain@users.noreply.github.com>
2026-03-08 22:40:03 +0600

    Add constructor

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,4 +1,11 @@
 public class MyStack {
     private int stackPointer;
     private int[] elements;
+
+    public MyStack() {
+        elements = new int[10];
+        stackPointer = 0;
+    }
+
+
 }
